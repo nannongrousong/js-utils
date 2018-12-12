72a9747b0492f07511985af3e70123332b5c4fb4
nannongrousong <37995042+nannongrousong@users.noreply.github.com>
2018-12-12 14:04:20 +0800

    add apply

--- call&apply&bind.js
+++ call&apply&bind.js
@@ -3,7 +3,7 @@ let earth = {
     welcome: function (name1, name2) {
         console.log(`welcome to ${this.areaName},${name1} and ${name2}`);
     }
-}
+};
 
 let school = {
     areaName: 'school'
@@ -15,7 +15,19 @@ Function.prototype.call2 = function (ctx) {
     ctx.fn = this;
     let args = Array(arguments.length - 1).fill('').map((v, i) => (`arguments[${i + 1}]`));
     eval(`ctx.fn(${args.toString()})`);
-    delete ctx.fn
-}
+    delete ctx.fn;
+};
 
-earth.welcome.call2(school, 'zhangsan', 'lisi')
+earth.welcome.call2(school, 'zhangsan', 'lisi');
+
+
+earth.welcome.apply(school, ['zhangsan', 'lisi']);
+
+Function.prototype.apply2 = function (ctx) {
+    ctx.fn = this;
+    let args = Array((arguments[1] || []).length).fill('').map((v, i) => (`arguments[1][${i}]`));
+    eval(`ctx.fn(${args.toString()})`);
+    delete ctx.fn;
+};
+
+earth.welcome.apply2(school, ['zhangsan', 'lisi']);
