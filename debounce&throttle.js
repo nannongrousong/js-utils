85eaad339905e6b74317bf2e0ba9bb6eafe90804
nannongrousong <37995042+nannongrousong@users.noreply.github.com>
2018-12-13 10:28:12 +0800

    Create debounce&throttle.js

---
+++ debounce&throttle.js
@@ -0,0 +1,30 @@
+function debounce(func, delay) {
+    let timeID = undefined;
+    return function () {
+        if (timeID) {
+            clearTimeout(timeID);
+        }
+
+        timeID = setTimeout(func.bind(this, arguments), delay);
+    }
+}
+
+function throttle(func, delay) {
+    let startTime = new Date().getTime();
+
+    return function () {
+        let presentTime = new Date().getTime();
+        if (presentTime - startTime > delay) {
+            func.apply(this, arguments);
+            startTime = new Date().getTime();
+        }
+    }
+}
+
+const eventLog = (e) => { console.log('e', e); console.log(`hihi_____${Math.random() * 100}`) };
+
+window.onload = () => {
+    const body = document.querySelector('body');
+    //body.addEventListener('mousemove', debounce(eventLog, 1000));
+    body.addEventListener('mousemove', throttle(eventLog, 1000));
+}
