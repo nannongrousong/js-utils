864adeb7b02b3ad3b4176511fb427f12c8843178
nannongrousong <37995042+nannongrousong@users.noreply.github.com>
2018-12-13 10:27:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,12 @@ function debounce(func, delay) {
             clearTimeout(timeID);
         }
 
-        let context = this;
-        let args = arguments;
-
-        timeID = setTimeout(func.bind(context, args), delay);
+        timeID = setTimeout(func.bind(this, arguments), delay);
     }
 }
 
 function throttle(func, delay) {
-    let startTime = undefined;
-    if (!startTime) {
-        startTime = new Date().getTime();
-    }
+    let startTime = new Date().getTime();
 
     return function () {
         let presentTime = new Date().getTime();
@@ -31,7 +25,6 @@ const eventLog = (e) => { console.log('e', e); console.log(`hihi_____${Math.rand
 
 window.onload = () => {
     const body = document.querySelector('body');
-    body.addEventListener('mousemove', debounce(eventLog, 1000));
-    //body.addEventListener('mousemove', throttle(eventLog, 1000));
+    //body.addEventListener('mousemove', debounce(eventLog, 1000));
+    body.addEventListener('mousemove', throttle(eventLog, 1000));
 }
-
