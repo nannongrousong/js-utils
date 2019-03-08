f0b0b9f3cf87fcee08951b3813b9623d68b14813
nannongrousong <37995042+nannongrousong@users.noreply.github.com>
2019-03-08 16:01:54 +0800

    使用ES6 扩展运算符优化

--- call&apply&bind.js
+++ call&apply&bind.js
@@ -1,47 +1,44 @@
 let earth = {
     areaName: 'earth',
-    welcome: function (name1, name2) {
-        console.log(`welcome to ${this.areaName},${name1} and ${name2}`);
+    welcome: function (name1, name2, nameArr) {
+        console.log(`welcome to ${this.areaName},${name1} and ${name2}, ${nameArr.toString()}`);
     }
 };
 
 let school = {
     areaName: 'school'
-}
+};
 
-earth.welcome.call(school, 'zhangsan', 'lisi')
-
+earth.welcome.call(school, 'zhangsan', 'lisi', ['wanger', 'wangliu']);
 Function.prototype.call2 = function (ctx) {
-    ctx.fn = this;
-    let args = Array(arguments.length - 1).fill('').map((v, i) => (`arguments[${i + 1}]`));
-    eval(`ctx.fn(${args.toString()})`);
-    delete ctx.fn;
+    const vFn = Symbol('vFn');
+    ctx[vFn] = this;
+    ctx[vFn](...[...arguments].slice(1));
+    delete ctx[vFn];
 };
+earth.welcome.call2(school, 'zhangsan', 'lisi', ['wanger', 'wangliu']);
 
-earth.welcome.call2(school, 'zhangsan', 'lisi');
-
-
-earth.welcome.apply(school, ['zhangsan', 'lisi']);
-
+earth.welcome.apply(school, ['zhangsan', 'lisi', ['wanger', 'wangliu']]);
 Function.prototype.apply2 = function (ctx) {
-    ctx.fn = this;
-    let args = Array((arguments[1] || []).length).fill('').map((v, i) => (`arguments[1][${i}]`));
-    eval(`ctx.fn(${args.toString()})`);
-    delete ctx.fn;
+    const vFn = Symbol('vFn');
+    ctx[vFn] = this;
+    if (arguments.length == 1) {
+        ctx[vFn]();
+    } else {
+        ctx[vFn](...[...arguments].slice(1)[0]);
+    }
+    delete ctx[vFn];
 };
-earth.welcome.apply2(school, ['zhangsan', 'lisi']);
+earth.welcome.apply2(school, ['zhangsan', 'lisi', ['wanger', 'wangliu']]);
 
-
-earth.welcome.bind(school, 'zhangsan', 'lisi')();
-
+earth.welcome.bind(school, 'zhangsan', 'lisi', ['wanger', 'wangliu'])();
 Function.prototype.bind2 = function (ctx) {
-    ctx.fn = this;
-    let arguments2 = arguments;
-
+    const vFn = Symbol('vFn');
+    ctx[vFn] = this;
+    let args = arguments;
     return function () {
-        let args = Array(arguments2.length - 1).fill('').map((v, i) => (`arguments2[${i + 1}]`));
-        eval(`ctx.fn(${args.toString()})`);
+        ctx[vFn](...[...args].slice(1));
+        delete ctx[vFn];
     };
 }
-
-earth.welcome.bind2(school, 'zhangsan', 'lisi')();
+earth.welcome.bind2(school, 'zhangsan', 'lisi', ['wanger', 'wangliu'])();
