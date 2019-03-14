84e889fe2bfe26a8aba1a5e99b2506ee3852d0bd
nannongrousong <37995042+nannongrousong@users.noreply.github.com>
2019-03-14 16:08:36 +0800

    函数组合合并

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 //  函数组合合并 compose(f, g, h)(...args) 等同于 f(g(h(...args)))
-const composeFunc = (...funcs) => funcs.reduce((prev, curr) => (...args) => prev(curr(...args)));
+const composeFunc = (...funcs) {
+  if (funcs.length === 0) {
+    return arg => arg
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0]
+  }
+
+  return funcs.reduce((a, b) => (...args) => a(b(...args)))
+}
 
 const fun1 = (a) => (a + 1);
 const fun2 = (a) => (a + 2);
