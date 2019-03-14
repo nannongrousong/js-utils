57ce7d1d7b42d91fee78e3ea32384546cf24b025
nannongrousong <37995042+nannongrousong@users.noreply.github.com>
2019-03-14 13:39:33 +0800

    函数组合合并

---
+++ index.js
@@ -0,0 +1,9 @@
+//  函数组合合并
+const composeFunc = (...funcs) => funcs.reduce((prev, curr) => (...args) => prev(curr(...args)));
+
+const fun1 = (a) => (a + 1);
+const fun2 = (a) => (a + 2);
+const fun3 = (a) => (a + 3);
+
+const lastFun = composeFunc(fun1, fun2, fun3)
+lastFun(10)
