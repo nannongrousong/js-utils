11902b8f7d1a0948acb21429908ee3d41ccc1db4
nannongrousong <37995042+nannongrousong@users.noreply.github.com>
2019-06-05 15:09:38 +0800

    支持函数返回值，使用扩展运算符简化代码

--- call&apply&bind.js
+++ call&apply&bind.js
@@ -2,6 +2,7 @@ let earth = {
     areaName: 'earth',
     welcome: function (name1, name2, nameArr) {
         console.log(`welcome to ${this.areaName},${name1} and ${name2}, ${nameArr.toString()}`);
+        return 'this is welcome function';
     }
 };
 
@@ -10,35 +11,35 @@ let school = {
 };
 
 earth.welcome.call(school, 'zhangsan', 'lisi', ['wanger', 'wangliu']);
-Function.prototype.call2 = function (ctx) {
-    const vFn = Symbol('vFn');
-    ctx[vFn] = this;
-    ctx[vFn](...[...arguments].slice(1));
-    delete ctx[vFn];
+Function.prototype.call2 = function (ctx, ...args) {
+	const vFn = Symbol('vFn');
+	ctx[vFn] = this;
+	const res = ctx[vFn](...args);
+	delete ctx[vFn];
+	return res;
 };
 earth.welcome.call2(school, 'zhangsan', 'lisi', ['wanger', 'wangliu']);
 
 earth.welcome.apply(school, ['zhangsan', 'lisi', ['wanger', 'wangliu']]);
-Function.prototype.apply2 = function (ctx) {
-    const vFn = Symbol('vFn');
-    ctx[vFn] = this;
-    if (arguments.length == 1) {
-        ctx[vFn]();
-    } else {
-        ctx[vFn](...[...arguments].slice(1)[0]);
-    }
-    delete ctx[vFn];
+Function.prototype.apply2 = function (ctx, ...args) {
+	const vFn = Symbol('vFn');
+	ctx[vFn] = this;
+	const res = args.length === 0
+		? ctx[vFn]()
+		: ctx[vFn](...args[0]);
+	delete ctx[vFn];
+	return res;
 };
 earth.welcome.apply2(school, ['zhangsan', 'lisi', ['wanger', 'wangliu']]);
 
 earth.welcome.bind(school, 'zhangsan', 'lisi', ['wanger', 'wangliu'])();
-Function.prototype.bind2 = function (ctx) {
-    const vFn = Symbol('vFn');
-    ctx[vFn] = this;
-    let args = arguments;
-    return function () {
-        ctx[vFn](...[...args].slice(1));
-        delete ctx[vFn];
-    };
+Function.prototype.bind2 = function (ctx, ...args) {
+	const vFn = Symbol('vFn');
+	ctx[vFn] = this;
+	return function () {
+		const res = ctx[vFn](...args);
+		delete ctx[vFn];
+		return res;
+	};
 }
 earth.welcome.bind2(school, 'zhangsan', 'lisi', ['wanger', 'wangliu'])();
